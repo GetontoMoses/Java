119da2e6703b6147cec7447b22b3c588c8fde8ab
Getonto3 <getontomoses18@gmail.com>
2023-10-23 14:35:20 +0300

    ones and twos on arrays

---
+++ Java_Codes/ArrayS.java
@@ -0,0 +1,8 @@
+public class ArrayS {
+    public static void main(String[] args){
+       String[] names={"David", "Qian", "Emina",
+       "Jamal", "Ashenafi" };
+    for(int i=0; i<names.length;i++){
+    System.out.println("Hello "+ names[i] + "."); }
+    }
+}
---
+++ Java_Codes/Main_2.java
@@ -0,0 +1,6 @@
+public class Main_2 {
+   public static void main(String[] args){
+    int[] [] matrix= {{20,30},{39,30}};
+    System.out.println(matrix);
+   } 
+}
