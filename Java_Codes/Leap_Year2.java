788b8dbf9dfc6605d07dafc3924fe7c0e6102197
Getonto3 <getontomoses18@gmail.com>
2023-10-24 15:09:22 +0300

    October 24th class

---
+++ Java_Codes/Leap_Year2.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Leap_Year2 {
+    public static void main(String[] args){
+    int year;
+       for(year=2000;year<=2030;year+=4){
+       System.out.println(year);
+       }
+    }
+}
---
+++ Java_Codes/Multiples_of_9.java
@@ -0,0 +1,8 @@
+public class Multiples_of_9 {
+    public static void main(String[] arg){
+        System.out.println("This are the multiples of 9 between 0 and 300" );
+        for(int i=9;i<300;i+=9){
+        System.out.println(i);
+    }
+}
+}
