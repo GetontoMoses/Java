2445a3d263258403ce50c1f046dd3ee41a99811b
Getonto3 <getontomoses18@gmail.com>
2023-10-24 10:57:58 +0300

    For and While loops

---
+++ Java_Codes/Break.java
@@ -0,0 +1,10 @@
+public class Break {
+    public static void main(String[] args){
+        for(int m = 5; m < 10; m++) {
+            if(m == 7) {
+                break;
+            }
+            System.out.println(m);
+        }
+    }
+}
---
+++ Java_Codes/Continue.java
@@ -0,0 +1,11 @@
+public class Continue {
+    public static void main(String[] args){
+        for(int m = 5; m < 10; m++) {
+            if(m == 7) {
+                continue;
+            }
+            System.out.println(m);
+        }
+    }
+    
+}
